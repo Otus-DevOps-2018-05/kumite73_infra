847efb7d9872856391fa33e63b2209edc94eaa3a
Aleksei Kiselev <kumite73@gmail.com>
2018-07-04 22:18:14 +0300

    Ansible-1 and task with *

---
+++ ansible/inventory.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require 'open3'
+require 'json'
+
+$VERBOSE=nil
+
+begin
+  app_ip, status = Open3.capture3("cd ../terraform/stage; terraform output app_external_ip;")
+  db_ip, status = Open3.capture3("cd ../terraform/stage; terraform output db_external_ip;")
+  if ARGV[0] == '--list'
+    j = {appserver: {hosts: [app_ip.strip]}, dbserver: {hosts: [db_ip.strip]},'_meta' => {'hostvars': {}}}
+  else
+    j = {app: {hosts: {appserver: {ansible_host: app_ip.strip}}}, db: {hosts: {dbserver: {ansible_host: db_ip.strip}}},'_meta' => {'hostvars': {}}}
+  end 
+  puts JSON.pretty_generate(j)
+rescue
+  p 'Error'
+end
