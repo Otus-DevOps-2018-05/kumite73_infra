93f26aa67dd1200b29a9dc224b4b36461c466b66
Aleksei Kiselev <kumite73@gmail.com>
2018-07-12 17:29:23 +0300

    Ansible-1 and Ansible-1*

--- ansible/inventory.rb
+++ ansible/inventory.rb
@@ -7,13 +7,19 @@ require 'json'
 $VERBOSE=nil
 
 begin
-  app_ip, status = Open3.capture3("cd ../terraform/stage; terraform output app_external_ip;")
-  db_ip, status = Open3.capture3("cd ../terraform/stage; terraform output db_external_ip;")
+  app_ip, stderr_str, status = Open3.capture3("cd ../terraform/stage; terraform output app_external_ip;")
+  db_ip,  stderr_str, status = Open3.capture3("cd ../terraform/stage; terraform output db_external_ip;")
   if ARGV[0] == '--list'
-    j = {app: {hosts: [app_ip.strip]}, db: {hosts: [db_ip.strip]},'_meta' => {'hostvars': {}}}
+    j = {app: {hosts: [app_ip.strip]}, 
+         db:  {hosts: [db_ip.strip]},
+         '_meta' => {'hostvars': {}}
+        }
   else
-    j = {app: {hosts: {appserver: {ansible_host: app_ip.strip}}}, db: {hosts: {dbserver: {ansible_host: db_ip.strip}}},'_meta' => {'hostvars': {}}}
-  end 
+    j = {app: {hosts: {appserver: {ansible_host: app_ip.strip}}},
+	 db:  {hosts: {dbserver: {ansible_host: db_ip.strip}}},
+         '_meta' => {'hostvars': {}}
+        }
+  end
   puts JSON.pretty_generate(j)
 rescue
   p 'Error'
